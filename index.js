39909fe700e46c75aa672d3265dfb4801136c775
TanishqSingla <tanishqsingla00@gmail.com>
2020-07-28 15:01:32 +0530

    Adding google code for token and credentials

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const { google } = require("googleapis");
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
 const TOKEN_PATH = "token.json";
+
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client file", err);
+
+  authorize(JSON.parse(content), authorizedCallback);
+});
+
+//Credentials authorization
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+}
+
+//checking for previous tokens
+fs.readFile(TOKEN_PATH, (err, token) => {
+  if (err) return getNewToken(oAuth2Client, callback);
+  oAuth2Client.setCredentials(JSON.parse(token));
+  callback(oAuth2Client);
+});
+
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorizing by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here:", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, Token) => {
+      if (err)
+        return console.log("Error while trying to retrieve access token", err);
+      oAuth2Client.setCredentials(token);
+      //store token to disk
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
